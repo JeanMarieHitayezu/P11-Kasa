b05ce2d5457cd4ebe5ff52b57b3416bd592cffb5
Jean Marie Hitayezu <hitayezujeanmarie@gmail.com>
2022-09-19 14:58:13 +0200

    Addition of the Card.js file

--- kasa/src/App.js
+++ kasa/src/App.js
@@ -4,6 +4,7 @@ import Layout from "@/layouts/Layout";
 import Error404 from "@/pages/404/404";
 import Home from "@/pages/Home/Home";
 import About from "@/pages/About/About";
+import Card from '@/pages/Lodging/Card';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
             <Route path="/" element={<Home />}/>
             <Route path="*" element={<Error404 />}/>
             <Route path="/a-propos" element={<About />}/>
+            <Route path="/logement/:id" element={<Card />}/>
 
             
           </Routes>
